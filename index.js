1483ffbb31d5a3757de08ba0003155d14cab7f5f
Nii <bnstephens001@gmail.com>
2022-06-10 09:19:59 +0000

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+        let num1 = 62;
+        let num2 = 1;  
+let  multiply = num1 * num2      
+
+
+let random = 1;
+ random > 0 
+
+ let num3 = 12;
+ let num4 = 3;
+
+ let mod = num3/num4 
+ 
+  
+// Checking the maximum number 
+let max = Math.max(15,16,17,18,19,20)
